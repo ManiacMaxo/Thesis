d87d17611da563bba1ad98c0d2bbf639df6edf5f
Victor <victor@gorchilov.com>
2021-01-09 17:45:57 +0200

    [fix] auc true false postive negative

--- evaluate.py
+++ evaluate.py
@@ -2,11 +2,18 @@ import numpy as np
 from tensorflow.keras import Model
 
 def AUC(model, X: np.ndarray, y: np.ndarray, tolerance=5):
-    correct = []
-    for i in range(len(X)): # if close to 5 -> no transcription
+    a = []
+    for i in range(len(X)): # if close to tolerance -> no transcription
         pred = not np.allclose(model.predict(np.expand_dims(X[i], 0)), 0, atol=tolerance)
         real = not np.allclose(y[i], 0, atol=tolerance)
 #         correct.append('correctly predicted' if pred == real else 'incorrectly predicted')
-        correct.append(pred == real)
+    if pred == real and pred == True:
+        correct.append('true positive')
+    elif pred == real and pred == False:
+        correct.append('true negative')
+    elif pred != real and pred == True:
+        correct.append('false positive')
+    elif pred != real and pred == False:
+        correct.append('false negative')
 
     return correct
