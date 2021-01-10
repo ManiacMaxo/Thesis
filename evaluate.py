70835fa55923c3a744d82b480189c914fa647473
Victor Gorchilov <victor@gorchilov.com>
2021-01-10 17:31:38 +0200

    [up] AUC evaluation

--- evaluate.py
+++ evaluate.py
@@ -1,19 +1,26 @@
 import numpy as np
 from tensorflow.keras import Model
 
-def AUC(model, X: np.ndarray, y: np.ndarray, tolerance=5):
-    a = []
-    for i in range(len(X)): # if close to tolerance -> no transcription
-        pred = not np.allclose(model.predict(np.expand_dims(X[i], 0)), 0, atol=tolerance)
-        real = not np.allclose(y[i], 0, atol=tolerance)
-#         correct.append('correctly predicted' if pred == real else 'incorrectly predicted')
-    if pred == real and pred == True:
-        correct.append('true positive')
-    elif pred == real and pred == False:
-        correct.append('true negative')
-    elif pred != real and pred == True:
-        correct.append('false positive')
-    elif pred != real and pred == False:
-        correct.append('false negative')
-
+def AUC(model, X: np.ndarray, y: np.ndarray, tolerance=3, output=dict):
+    if (len(X.shape) < 3):
+        X = np.expand_dims(X, 0)
+    correct = []
+    predictions = model.predict(X)
+    
+    for i in range(len(X)):
+        pred = not np.allclose(predictions[i], tolerance/2, atol=tolerance)
+        real = not np.allclose(y[i], tolerance/2, atol=tolerance)
+        same = pred == real
+        if same and pred == True:
+            correct.append('true positive')
+        elif same and pred == False:
+            correct.append('true negative')
+        elif not same and pred == True:
+            correct.append('false positive')
+        elif not same and pred == False:
+            correct.append('false negative')
+            
+    if (output == dict):
+        freq = [correct.count(entry) for entry in correct]
+        return dict(list(zip(correct, freq)))
     return correct
