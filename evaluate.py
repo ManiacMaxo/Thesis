1480f1f36ddbb9e3aae73b1851ab4b884c66f258
Victor Gorchilov <victor@gorchilov.com>
2021-01-11 22:07:56 +0200

    [fix] auc function

--- evaluate.py
+++ evaluate.py
@@ -2,15 +2,16 @@ import numpy as np
 from tensorflow.keras import Model
 
 def AUC(model, X: np.ndarray, y: np.ndarray, tolerance=3, output=dict):
+    assert len(X) == len(y)
     if (len(X.shape) < 3):
         X = np.expand_dims(X, 0)
     correct = []
     predictions = model.predict(X)
     
     for i in range(len(X)):
-        pred = not np.allclose(predictions[i], tolerance/2, atol=tolerance)
-        real = not np.allclose(y[i], tolerance/2, atol=tolerance)
-        same = pred == real
+        pred = (predictions[i] < tolerance).all()
+        real = (y[i] < tolerance).all()
+        same = (pred == real)
         if same and pred == True:
             correct.append('true positive')
         elif same and pred == False:
@@ -19,7 +20,7 @@ def AUC(model, X: np.ndarray, y: np.ndarray, tolerance=3, output=dict):
             correct.append('false positive')
         elif not same and pred == False:
             correct.append('false negative')
-            
+
     if (output == dict):
         freq = [correct.count(entry) for entry in correct]
         return dict(list(zip(correct, freq)))
